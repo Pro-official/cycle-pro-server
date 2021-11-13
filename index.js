3a6d1f2794b393bfd1ee8e9a5d3410313a8ebf34
Pro-official <promise.iam00@gmail.com>
2021-11-13 12:53:17 +0600

    edited again

--- index.js
+++ index.js
@@ -1,43 +1,110 @@
 const express = require("express");
-const app = express();
+const { MongoClient } = require("mongodb");
+const ObjectId = require("mongodb").ObjectId;
 const cors = require("cors");
 require("dotenv").config();
-const { MongoClient } = require("mongodb");
 
+const app = express();
 const port = process.env.PORT || 5000;
 
+// MIDDLEWARE
 app.use(cors());
 app.use(express.json());
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.swu9d.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
-
+// SETTING UP DATABASE
+const uri = `mongodb+srv://final:t9xu6F9h46PS7wCr@cluster0.j6ced.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
 
+// MAIN INITIALIZATION
 async function run() {
   try {
     await client.connect();
-    const database = client.db("cycleDB");
+    const database = client.db("Dbcycle");
     const ordersCollection = database.collection("orders");
+    const productsCollection = database.collection("products");
 
-    // Find collection with email
-    // app.get("/appointments", async (req, res) => {
-    //   const email = req.query.email;
-    //   const date = new Date(req.query.date).toLocaleDateString();
-    //   const query = { email: email, date: date };
-    //   const cursor = appointmentsCollection.find(query);
-    //   const appointments = await cursor.toArray();
-    //   res.json(appointments);
-    // });
-
-    // Post new order
-    app.post("/orders", async (req, res) => {
-      const appointment = req.body;
-      const result = await ordersCollection.insertOne(appointment);
-      console.log(result);
+    // POST API
+    app.post("/products", async (req, res) => {
+      const newPlan = req.body;
+      const result = await productsCollection.insertOne(newPlan);
+      // console.log(newPlan);
       res.json(result);
+    });
+
+    //  GET API WHICH WAS PLACED ORDERED BY USER
+    app.get("/products", async (req, res) => {
+      const cursor = productsCollection.find({});
+      const products = await cursor.toArray();
+      console.log(products);
+      res.json(products);
+    });
+
+    app.get("/products/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const order = await productsCollection.findOne(query);
+      res.json(order);
+    });
+
+    //  GET API WHICH WAS PLACED ORDER BY USER WITH THEIR EMAIL
+    app.get("/products/:name", async (req, res) => {
+      const name = req.params.name;
+      const query = { name: name };
+      const order = await productsCollection.find(query);
+      console.log(order);
+      res.json(order);
+    });
+
+    // POST API IN PLANS
+    app.post("/orders", async (req, res) => {
+      const newPlan = req.body;
+      const result = await ordersCollection.insertOne(newPlan);
+      res.json(result);
+    });
+
+    // GET API
+    app.get("/orders", async (req, res) => {
+      const cursor = ordersCollection.find({});
+      const plans = await cursor.toArray();
+      res.send(plans);
+    });
+
+    // GET SINGLE API
+    app.get("/plans/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const plan = await ordersCollection.findOne(query);
+      res.json(plan);
+    });
+
+    // UPDATE PLAN
+    app.put("/products/:id", async (req, res) => {
+      const id = req.params.id;
+      const updatedUser = req.body;
+      const filter = { _id: ObjectId(id) };
+      const updateDoc = {
+        $set: {
+          name: updatedUser.name,
+          email: updatedUser.email,
+          address: updatedUser.address,
+          post: updatedUser.post,
+          status: updatedUser.status,
+        },
+      };
+      const result = await productsCollection.updateOne(filter, updateDoc);
+      console.log("update user", id);
+      res.json(result);
+    });
+
+    // DELETE PLAN
+    app.delete("/devplan/:id", async (req, res) => {
+      const result = await productsCollection.deleteOne({
+        _id: ObjectId(req.params.id),
+      });
+      res.send(result);
     });
   } finally {
     // await client.close();
@@ -47,9 +114,9 @@ async function run() {
 run().catch(console.dir);
 
 app.get("/", (req, res) => {
-  res.send("Hello Cycle Pro!");
+  res.send("Welcome to my site...");
 });
 
 app.listen(port, () => {
-  console.log(`listening at ${port}`);
+  console.log(`Example app listening at http://localhost:${port}`);
 });
